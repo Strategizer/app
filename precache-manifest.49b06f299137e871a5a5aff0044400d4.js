ebf3cd0af796e4d29c4ce33b9d3b4aaddd27a12e
Singh <swajeetxsingh@gmail.com>
2020-10-02 08:31:00 +0530

    Updates

---
+++ precache-manifest.49b06f299137e871a5a5aff0044400d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25924c256ce430ff096563f058ffd483",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "d5761a7a18a9b4909964",
+    "url": "/app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56ad578a02316beb06ec",
+    "url": "/app/static/js/2.1c5ba451.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.1c5ba451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5761a7a18a9b4909964",
+    "url": "/app/static/js/main.8e519005.chunk.js"
+  },
+  {
+    "revision": "3097480c0b23236ce8e9",
+    "url": "/app/static/js/runtime-main.231ab319.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
